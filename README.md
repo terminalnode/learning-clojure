391e74c0b733799d7daf9c4c34f3fac88c02b4a6
Alexander <alexanderrundberg@fastmail.jp>
2020-09-20 20:32:00 +0200

    add hobbitbuilder reducing method and hit method
    The reducing method is a different approach to completing the hobbit
    body. The hit method is a way to select a random body part of the hobbit
    with bigger parts having a higher likelihood of getting hit.
    
    Also added a readme.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning Clojure
+These are my notes and exercises as I move through Daniel Higginbotham's book
+[Clojure for the Brave and True](https://www.braveclojure.com/clojure-for-the-brave-and-true/).
--- ch3-hobbitbuilder/src/hobbitbuilder/core.clj
+++ ch3-hobbitbuilder/src/hobbitbuilder/core.clj
@@ -33,10 +33,10 @@
   "Takes a map representing a body part, with :name and :size, and returns the other side of it."
   [part]
   (if (re-find #"^left" (:name part))
-  { :name (clojure.string/replace (:name part) #"^left" "right")
-    :size (:size part) }
-  { :name (clojure.string/replace (:name part) #"^right" "left")
-    :size (:size part) }))
+    { :name (clojure.string/replace (:name part) #"^left" "right")
+      :size (:size part) }
+    { :name (clojure.string/replace (:name part) #"^right" "left")
+      :size (:size part) }))
 
 (defn symmetrize-body-parts
   "Expects a seq of maps representing body parts, with :name and :size"
@@ -48,6 +48,29 @@
         (recur remaining
                (into final-body-parts
                      (set [part (get-matching-part part)])))))))
+
+;; Same function, but using reduce instead, which is much cooler
+(defn reduce-body-parts
+  "Expects a seq of maps representing body parts, with :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+    (into final-body-parts (set [part (get-matching-part part)])))
+    []
+    asym-body-parts))
+
+(defn hit
+  [body-parts]
+  (let [body-part-size-sum (reduce + (map :size body-parts))
+        target (rand body-part-size-sum)]
+    (loop [[part & remaining] body-parts
+           accumulated-size (:size part)]
+      (if (>= accumulated-size target)
+        part
+        (recur remaining (+ accumulated-size (:size (first remaining))))))))
+
+(defn hit-asym
+  [asym-body-parts]
+  (hit (reduce-body-parts asym-body-parts)))
 
 (defn -main
   "I don't do a whole lot ... yet."
